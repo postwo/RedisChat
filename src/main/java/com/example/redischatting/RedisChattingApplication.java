edcc05a7c56e62ad4a59af9268b1f37b378ff6e2
postwo <codo7717@naver.com>
2024-06-30 22:34:25 +0900

    간단 redis chat 완료

--- src/main/java/com/example/redischatting/RedisChattingApplication.java
+++ src/main/java/com/example/redischatting/RedisChattingApplication.java
@@ -1,11 +1,17 @@
 package com.example.redischatting;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class RedisChattingApplication implements CommandLineRunner {
+
+    @Autowired
+    private  ChatService chatService;
+
 
     public static void main(String[] args) {
         SpringApplication.run(RedisChattingApplication.class, args);
@@ -15,5 +21,6 @@ public class RedisChattingApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("appliction started");
+        chatService.enterCharRoom("ch1");
     }
 }
